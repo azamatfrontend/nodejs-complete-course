d5e767590e93f51cade0d8ee1f09e085a70be5e2
Azamat <imfrontenddev@gmai.com>
2022-05-28 02:33:47 +0500

    the nodejs lifecycle and event loop

---
+++ 3-the-node-lifecycle-and-event-loop/3-process-exit.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  console.log(req);
+  process.exit();
+});
+
+server.listen(3000, () => {
+  console.log("Listening on port 3000");
+  console.log("Listening on Press CTRL+Mouse http://localhost:3000");
+  console.log("Press Ctrl+C to quit.");
+});
